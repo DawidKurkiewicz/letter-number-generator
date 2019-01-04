3e4bffcce93c60cf286e1745d5b447b2163d71d9
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-04 14:37:21 +0100

    random works

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ document.body.appendChild(button)
 const text = document.createElement("div")
 document.body.appendChild(text)
 const btn = document.querySelector("button")
+const div = document.querySelector("div")
+const numbers = ['1', '2', '3', '4', '5', '6', '7']
 const generator = () => {
-
+const index = Math.floor(Math.random() * 7)
+div.innerHTML = `a ${numbers[index]}`
 }
-btn.addEventListener( "click", generator)
+btn.addEventListener("click", generator)
