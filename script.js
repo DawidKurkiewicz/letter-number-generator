b48a4c7eee58faa71ad0f7e8bc994c55b4115497
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-04 15:03:19 +0100

    totally random generator

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ const text = document.createElement("div")
 document.body.appendChild(text)
 const btn = document.querySelector("button")
 const div = document.querySelector("div")
-const numbers = ['1', '2', '3', '4', '5', '6', '7']
-const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 const generator = () => {
+    let number = "";
+    const numbers = "1234567890";
+    while (numbers.length < 1) {
+        numbers += alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    let letters = "";
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    while (letters.length < 1) {
+        letters += alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
     const indexNum = Math.floor(Math.random() * numbers.length)
-    // const indexLet = Math.floor(Math.random() * letters.length)
-    // div.innerHTML = `${letters[indexLet]} ${numbers[indexNum]}`
     div.innerHTML = `${letters[Math.floor(Math.random() * letters.length)]} ${numbers[indexNum]}`
 }
 btn.addEventListener("click", generator)
+
