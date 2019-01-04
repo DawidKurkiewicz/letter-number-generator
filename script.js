88ce7247b2e2c1e24535f86cd0e012ebe045ee5e
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-04 14:04:02 +0100

    added styles and basic js

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.createElement("button")
+button.innerHTML = "random"
+document.body.appendChild(button)
+const text = document.createElement("div")
+document.body.appendChild(text)
+const btn = document.querySelector("button")
+const generator = () => {
+
+}
+btn.addEventListener( "click", generator)
