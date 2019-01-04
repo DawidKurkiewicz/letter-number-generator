b29e3faf5f2933ef4bd1ec81bef0335f8d948d94
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-04 14:52:23 +0100

    generator works

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const div = document.querySelector("div")
 const numbers = ['1', '2', '3', '4', '5', '6', '7']
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 const generator = () => {
-const indexNum = Math.floor(Math.random() * numbers.length)
-const indexLet = Math.floor(Math.random() * letters.length)
-
-div.innerHTML = `${letters[indexLet]} ${numbers[indexNum]}`
+    const indexNum = Math.floor(Math.random() * numbers.length)
+    // const indexLet = Math.floor(Math.random() * letters.length)
+    // div.innerHTML = `${letters[indexLet]} ${numbers[indexNum]}`
+    div.innerHTML = `${letters[Math.floor(Math.random() * letters.length)]} ${numbers[indexNum]}`
 }
 btn.addEventListener("click", generator)
